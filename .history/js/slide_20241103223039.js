1a0e4f8ede7f05e8c79f3b0f670f13ba4891b005
dlgywls1130 <dlgywls1130@gmail.com>
2024-11-07 21:14:48 +0900

    mobile size

---
+++ .history/js/slide_20241103223039.js
@@ -0,0 +1,5 @@
+var swiper = new Swiper(".middle-slide", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
---
+++ .history/js/slide_20241103235727.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".middle-slide", {
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+  });
